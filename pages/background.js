b89035e2ac1c3a73a6f90f743c107c537c247f9a
wahidari <wahiid.ari@gmail.com>
2023-10-01 17:33:12 +0700

    UPDATE: Add Background page

--- pages/background.js
+++ pages/background.js
@@ -23,7 +23,7 @@ export default function Background() {
 
       <Navbar />
 
-      <Layout className="relative bg-neutral-">
+      <Layout className="relative bg-neutral-900">
 
         <main className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 py-8">
 
@@ -32,8 +32,8 @@ export default function Background() {
             <Image className="absolute z-0 right-0 top-1/2" src="/ilustration/particle-right.svg" width={150} height={150} alt="illustration" />
             <div className="absolute z-0 rounded-full blur-[90px] opacity-20 animate-pop w-[300px] h-[300px] bg-orange-500 translate-y-[50px] left-0"></div>
             <div className="absolute z-0 rounded-full blur-[90px] opacity-20 animate-pop lg:w-[500px] lg:h-[500px] bg-emerald-500 top-0 lg:translate-x-80"></div>
-            <div className="absolute z-0 right-0 rounded-full opacity-20 animate-blob blur-[90px] w-[350px] h-[350px] bg-red-600 -translate-x-[100px]"></div>
-            <div className="w-full relative justify-center flex z-10">
+            <div className="absolute z-0 right-0 rounded-full opacity-20 lg:animate-blob blur-[90px] w-[350px] h-[350px] bg-red-600 -translate-x-[100px]"></div>
+            <div className="w-full relative justify-center flex z-[1]">
               <Image src="/ilustration/maintenance.svg" width={400} height={400} alt="illustration" />
             </div>
           </div>
@@ -42,29 +42,29 @@ export default function Background() {
             <div className="relative">
               <Image className="absolute z-0 top-1/2" src="/ilustration/particle-left.svg" width={150} height={150} alt="illustration" />
               <div className="absolute z-0 rounded-full blur-[90px] opacity-20 animate-pop w-[300px] h-[300px] bg-orange-600 translate-y-[150px]"></div>
-              <div className="absolute z-0 right-0 rounded-full opacity-20 animate-blob blur-[120px] w-[350px] h-[350px] bg-emerald-600 -translate-x-[100px]"></div>
+              <div className="absolute z-0 right-0 rounded-full opacity-20 lg:animate-blob blur-[120px] w-[350px] h-[350px] bg-emerald-600 -translate-x-[100px]"></div>
               <div className="w-full relative justify-center flex z-10">
                 <Image src="/ilustration/how-we-work.svg" width={400} height={400} alt="illustration" />
               </div>
             </div>
-            <div className="dark:text-white flex flex-col justify-center z-10 relative">
+            <div className="text-white flex flex-col justify-center z-10 relative">
               <div className="absolute z-0 rounded-full blur-[120px] opacity-20 animate-pop w-[400px] h-[400px] bg-sky-500 right-0 top-0"></div>
               <p className="font-bold text-3xl mb-8">Everything is well planned, well designed and developed wholeheartedly Nostrud nostrud nostrud aliqua irure minim irure</p>
-              <p className="text-lg">Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped. Incididunt minim proident laborum dolor consequat. Labore excepteur ullamco duis labore incididunt cupidatat. </p>
+              <p className="text-lg text-neutral-200">Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped. Incididunt minim proident laborum dolor consequat. Labore excepteur ullamco duis labore incididunt cupidatat. </p>
             </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 py-16">
             <div className="text-white flex flex-col justify-center z-10 relative">
-              <div className="absolute z-0 rounded-full blur-[120px] opacity-20 animate-pop w-[400px] h-[400px] bg-yellow-600 top-0"></div>
+              <div className="absolute z-0 rounded-full blur-[120px] opacity-20 animate-pop w-[200px] h-[200px] lg:w-[400px] lg:h-[400px] bg-yellow-600 top-0"></div>
               <p className="font-bold text-3xl mb-8">Everything is well planned, well designed and developed wholeheartedly Nostrud nostrud nostrud aliqua irure minim irure</p>
-              <p className="text-lg">Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped. Incididunt minim proident laborum dolor consequat. Labore excepteur ullamco duis labore incididunt cupidatat. </p>
+              <p className="text-lg text-neutral-200">Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped. Incididunt minim proident laborum dolor consequat. Labore excepteur ullamco duis labore incididunt cupidatat. </p>
             </div>
             <div className="relative">
               <Image className="absolute z-0 right-0 top-1/2" src="/ilustration/particle-right.svg" width={150} height={150} alt="illustration" />
-              <div className="absolute z-0 rounded-full blur-[90px] opacity-20 animate-pop w-[300px] h-[300px] bg-pink-500 translate-x-[250px] translate-y-[150px]"></div>
-              <div className="absolute z-0 rounded-full opacity-20 animate-blob blur-[120px] w-[350px] h-[350px] bg-purple-700"></div>
-              <div className="w-full relative justify-center flex z-20">
+              <div className="absolute z-0 rounded-full blur-[90px] opacity-20 animate-pop w-[300px] h-[300px] bg-pink-500 lg:translate-x-[250px] lg:translate-y-[150px]"></div>
+              <div className="absolute z-0 rounded-full opacity-20 lg:animate-blob blur-[120px] w-[150px] h-[150px] lg:w-[350px] lg:h-[350px] bg-purple-700"></div>
+              <div className="w-full relative justify-center flex z-[1]">
                 <Image src="/ilustration/development.svg" width={400} height={400} alt="illustration" />
               </div>
             </div>
