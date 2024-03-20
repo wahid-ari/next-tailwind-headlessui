f71fe9b8ec3ad866f2c5bfe767e1887911555480
wahid-ari <wahiid.arii@gmail.com>
2024-03-20 23:14:33 +0700

    fix: pino log

--- utils/logger.js
+++ utils/logger.js
@@ -4,23 +4,23 @@ const logger = pino({
   browser: {},
   // to console pretty format, comment to show original log
   // stdTimeFunctions,
-  timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
+  // timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
   // "2024-03-20T15:55:15.691Z"
-  // timestamp: () => `,"time":"${new Date()}"`,
+  timestamp: () => `,"time":"${new Date()}"`,
   // "Wed Mar 20 2024 22:58:18 GMT+0700 (Indochina Time)"
   transport: {
     targets: [
-      {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          levelFirst: false,
-          // ignore: 'time,hostname,pid',
-          ignore: 'hostname',
-          translateTime: 'SYS:standard',
-          // translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o',
-        },
-      },
+      // {
+      //   target: 'pino-pretty',
+      //   options: {
+      //     colorize: true,
+      //     levelFirst: false,
+      //     // ignore: 'time,hostname,pid',
+      //     ignore: 'hostname',
+      //     translateTime: 'SYS:standard',
+      //     // translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o',
+      //   },
+      // },
       // if want to also write to log file, use the config below,
       // and create "logger.log" and "logger-pretty.log" file in root dir
       {
